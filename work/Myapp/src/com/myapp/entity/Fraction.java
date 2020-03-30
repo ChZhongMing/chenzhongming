e04b432fd6496da1a9d07a830c5b7fec7a6c3823
CZM <2723081630@qq.com>
2020-03-30 23:52:43 +0800

    完善分式类fraction并封装其加、减、乘、除、运算方法
    Signed-off-by: CZM <2723081630@qq.com>

--- work/Myapp/src/com/myapp/entity/Fraction.java
+++ work/Myapp/src/com/myapp/entity/Fraction.java
@@ -46,11 +46,144 @@ public class Fraction {
      */
     public void add(Fraction fraction) {
         this.integer += fraction.getInteger();
+        if (this.denominator == fraction.getDenominator()) {
+            this.molecule += fraction.getMolecule();
+        } else {
+            this.molecule = this.molecule * fraction.getDenominator() + fraction.getMolecule() * this.denominator;
+            this.denominator *= fraction.getDenominator();
+        }
+    }
+
+    /**
+     * 减法一个整数
+     *
+     * @param num
+     */
+    public void reduce(int num) {
+        this.integer -= num;
+    }
+
+    /**
+     * 减一个分式
+     *
+     * @param fraction
+     */
+    public void reduce(Fraction fraction) {
+        this.integer -= fraction.getInteger();
+        if (this.denominator == fraction.getDenominator()) {
+            this.molecule -= fraction.getMolecule();
+        } else {
+            this.molecule = this.molecule * fraction.getDenominator() - fraction.getMolecule() * this.denominator;
+            this.denominator *= fraction.getDenominator();
+        }
+
+        //分子小于等于0
+        while (this.molecule <= 0) {
+            this.molecule += this.denominator;
+            this.integer--;
+        }
+
+    }
+
+
+
+    /**
+     * 乘一个整数
+     *
+     * @param num
+     */
+    public void ride(int num) {
+        if (this.integer != 0) {
+            this.molecule += this.integer * this.denominator;
+            this.integer = 0;
+        }
+        this.molecule *= num;
+
+    }
+
+    /**
+     * 乘一个分式
+     *
+     * @param fraction
+     */
+    public void ride(Fraction fraction) {
+        if (this.integer != 0) {
+            this.molecule += this.integer * this.denominator;
+            this.integer = 0;
+        }
+        if(fraction.getInteger() != 0) {
+            fraction.setMolecule(fraction.getInteger() * fraction.getDenominator() +fraction.getMolecule());
+            fraction.setInteger(0);
+        }
+        this.molecule *= fraction.molecule;
+        this.denominator *= fraction.getDenominator();
+    }
+
+    /**
+     * 除以一个整数
+     * @param num
+     */
+    public void divide(int num){
+        this.molecule += this.integer * this.denominator;
+        this.integer = 0;
+        this.denominator *= num;
+
+    }
+
+    /**
+     * 除以一个分式
+     * @param fraction
+     */
+    public void divide(Fraction fraction){
+        if (this.integer != 0) {
+            this.molecule += this.integer * this.denominator;
+            this.integer = 0;
+        }
+        if(fraction.getInteger() != 0) {
+            fraction.setMolecule(fraction.getInteger() * fraction.getDenominator() +fraction.getMolecule());
+            fraction.setInteger(0);
+        }
+        this.molecule *= fraction.getDenominator();
+        this.denominator *= fraction.getMolecule();
+    }
+
+    /**
+     * 求两数最大公约数
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    public static int getCommonDivisor(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            int remainder = a % b;
+            a = b;
+            b = remainder;
+            return getCommonDivisor(a, b);
+        }
     }
 
     @Override
     public String toString() {
-        if (this.integer > 0) {
+        //化简
+        if (this.molecule > this.denominator) {
+            this.integer += this.molecule / this.denominator;
+            this.molecule = this.molecule % this.denominator;
+        }
+        //约分
+        int commonDivisor = getCommonDivisor(this.denominator, this.molecule);
+        this.denominator /= commonDivisor;
+        this.molecule /= commonDivisor;
+
+        if (this.molecule == 0){
+            //分子为0
+            return this.integer+"";
+        } else if (denominator == 1) {
+            //分母为1
+            return (this.integer + this.molecule) + "";
+        } else if (this.integer != 0) {
             return this.integer + "’" + this.molecule + "/" + denominator;
         } else {
             return this.molecule + "/" + denominator;
--- work/Myapp/test/com/myapp/MainTest.java
+++ work/Myapp/test/com/myapp/MainTest.java
@@ -1,12 +1,41 @@
 package com.myapp;
 
 
+import com.myapp.entity.Fraction;
 import org.junit.Test;
 
 public class MainTest {
 
     @Test
     public void main() {
-        Main.exercisesCheck("题目.txt", "答案.txt");
+//        Main.exercisesCheck("题目.txt", "答案.txt");
+//        System.out.println(Fraction.getCommonDivisor(2,2));
+//        System.out.println(new Fraction(0, 19, 6));
+        Fraction fraction = new Fraction(1, 16, 6);
+        Fraction fraction2 = new Fraction(1, 16, 6);
+        System.out.println("原值"+fraction);
+        //乘
+        fraction.ride(3);
+        System.out.println("乘3："+fraction);
+        fraction.divide(2);
+        System.out.println("除以2："+fraction);
+        fraction.ride(fraction2);
+        System.out.println("乘3’2/3："+fraction);
+        //除
+        fraction.divide(fraction2);
+        System.out.println("除以3’2/3："+fraction);
+        //减
+        fraction.reduce(10);
+        System.out.println("减10："+fraction);
+        fraction.reduce(fraction2);
+        System.out.println("减3’2/3："+fraction);
+        //加
+        fraction.add(fraction2);
+        System.out.println("加3’2/3："+fraction);
+        fraction.add(12);
+        System.out.println("加12："+fraction);
+
+        fraction.divide(2);
+        System.out.println("除以2："+fraction);
     }
 }
